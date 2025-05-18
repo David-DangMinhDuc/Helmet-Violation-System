1e8a7f87af370bdd06b2b4ac3d27ac93ee5a94fc
David-DangMinhDuc <112240023+David-DangMinhDuc@users.noreply.github.com>
2025-05-19 01:33:21 +0700

    Update trainForDetection.py (change hyperparameter)

--- trainForDetection.py
+++ trainForDetection.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO("yolo11n.pt")
-    results = model.train(data="/kaggle/input/helmetriderplate/data.yaml", epochs = 50, patience=0, batch=16, lr0=0.0005, save=True)
+    results = model.train(data="data.yaml", epochs = 100, batch=16, lr0=0.001, save=True)
